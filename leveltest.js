73877c62062c16fdee9b99d69b20ed551970aeac
SBHchic <SBHchic1@gmail.com>
2021-04-23 17:29:45 +0900

    라디오 문제에서 이전 질문관련 이슈 해결 + 누적 점수 추가

--- leveltest.js
+++ leveltest.js
@@ -19,25 +19,54 @@ document.querySelector('#btn_quiz_start').addEventListener('click', function(){
 //             alert('질문에 답해주세요.');
 //             return false;
 //         }
+
+//         // 선택한 값이 정답인지 아닌지 저장하는 배열에 값 넣기
+//         document.querySelectorAll('input[type=radio][name=qz' + current_idx + ']:checked').forEach(function(Node2){
+//             if (Node2.value == quiz_str[current_idx-1].answer_que){
+//                 test_result[current_idx-1] = true;
+//                 // 정답이라면 그 점수만큼 더하기
+//                 result_score += quiz_str[current_idx - 1].score;
+//             } else {
+//                 test_result[current_idx-1] = false;
+//             }
+//         });
+
 //         document.querySelector('.con_qz_' + current_idx).style.display='none';
 //         document.querySelector('.con_qz_' + (++current_idx)).style.display='block';
 //     });
-// })
+// });
 
 // explore에서도 돌아가는 문법
 let next = document.querySelectorAll('.btn_next') // 11개
 for (let i = 0; i < next.length; i++){
     next[i].addEventListener('click', function(){
         // 라디오 체크했는지 확인
-        let status = false;
-        let checked = document.querySelectorAll('input[type=radio][name=qz' + current_idx + ']');
-        for (let j = 0; j < checked.length; j++){
-            status = status || checked[j].checked;
-        }
-        if (!status) {// 라디오 체크 x 라면
+        // let status = false;
+        // let checked = document.querySelectorAll('input[type=radio][name=qz' + current_idx + ']');
+        // for (let j = 0; j < checked.length; j++){
+        //     status = status || checked[j].checked;
+        // }
+        // if (!status) {// 라디오 체크 x 라면
+        //     alert('질문에 답해주세요.');
+        //     return false;
+        // }
+
+        // 위의 코드 리팩토링
+        let checked = document.querySelectorAll('input[type=radio][name=qz'+current_idx+']:checked');
+        if (checked.length === 0){
             alert('질문에 답해주세요.');
             return false;
         }
+
+        // 선택한 값이 정답인지 아닌지 저장하는 배열에 값 넣기
+        if (checked[0].value == quiz_str[current_idx -1].answer_que){
+            test_result[current_idx -1] = true;
+            // 정답이라면 그 점수만큼 더하기
+            result_score += quiz_str[current_idx - 1].score;
+        } else{
+            test_result[current_idx -1] = false;
+        }
+
         document.querySelector('.con_qz_' + current_idx).style.display='none';
         document.querySelector('.con_qz_' + (++current_idx)).style.display='block';
     });
@@ -47,6 +76,10 @@ for (let i = 0; i < next.length; i++){
 // 라디오에서는 체크가 안풀리는 것을 확인하여 굳이 넣을 필요 없을 것 같음
 document.querySelectorAll('.btn_prev').forEach(function(o){
     o.addEventListener('click', function(){
+        // 뒤로갈 때 이전 문제가 정답이었다면 그 점수만큼 빼기
+        if (test_result[current_idx - 2]){
+            result_score -= quiz_str[current_idx - 2].score;
+        }
         document.querySelector('.con_qz_' + current_idx).style.display='none';
         document.querySelector('.con_qz_' + (--current_idx)).style.display='block';
     });
